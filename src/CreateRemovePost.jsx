b30a4b4614c9e22f7d056a9eae587f533aaf8a11
dankoknad <dankomilutinovic@gmail.com>
2017-04-01 16:46:23 +0200

    update

--- src/CreateRemovePost.jsx
+++ src/CreateRemovePost.jsx
@@ -44,7 +44,7 @@ export default function CreateRemovePost({posts, title, content, updateTitle, up
 			<hr/>
 			<div className="well">
 				<h3 className="text-danger">Danger zone!</h3>
-				<div className="alert alert-danger">Remove items: <span className="glyphicon glyphicon-remove hide"></span></div>
+				<div className="alert alert-danger">Remove posts: <span className="glyphicon glyphicon-remove hide"></span></div>
 				<ul className="list-group">
 					<ReactCSSTransitionGroup
 						transitionName="remove"
