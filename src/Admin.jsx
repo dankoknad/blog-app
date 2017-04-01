ecb2d0eb2c57a84ddbe2e583d1a39ab387c2e08b
dankoknad <dankomilutinovic@gmail.com>
2017-04-01 19:28:05 +0200

    updates

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -4,11 +4,12 @@ export default function Admin({children, toggleAdminTabs, activeTab}) {
 	return (
 		<div>
 			<h2 className="text-center">This is the Admin page</h2>
+			<br/>
 			<ul className="nav nav-tabs">
 				<li role="presentation" className={activeTab ? "active" : null}><a onClick={toggleAdminTabs} data-tab="1" href="#">Create / Remove Post</a></li>
 				<li role="presentation" className={!activeTab ? "active" : null}><a onClick={toggleAdminTabs} data-tab="0" href="#">Edit Post</a></li>
 			</ul>
-			<br/>
+			<br/><br/>
 			{children}			
 		</div>
 	)
